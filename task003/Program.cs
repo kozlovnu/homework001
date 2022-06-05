cfd633cc0657f459b8551c51e9b6cfb56df8bb0c
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 18:53:08 +0300

    Add task003 even numbers

---
+++ task003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Enter your number: ");
+double numberA = Convert.ToDouble(Console.ReadLine());
+double numberB = numberA % 2;
+if (numberB == 0) Console.WriteLine("Your number is even");
+if (numberB != 0) Console.WriteLine("Your number is odd");
+//Console.WriteLine(numberB);
