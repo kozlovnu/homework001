0dabeccc584464b231dcd3ad94a9d7f5411056b2
Nikita Kozlov <kozlov.nu@gmail.com>
2022-06-05 18:00:35 +0300

    Add the second task - find the max number

---
+++ task002_max_number/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Enter the first number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the third number: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (max < numberB) max = numberB;
+if (max < numberC) max = numberC;
+if (max < numberA) max = numberA;
+
+Console.Write("Max number = ");
+Console.WriteLine(max);
